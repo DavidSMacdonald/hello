ef4be012a9afeb0f71e9033110bc28d01f204149
David Macdonald <davidmacdonald@billericak12.com>
2021-09-10 11:09:56 -0400

    David Macdonald davidmacdonald@billericak12.com

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// it says hello and den gb
+        System.out.print("This is the beginning");
+        System.out.println("Hello, world!");
+        System.out.println("Goodbye");
+    }
+}
